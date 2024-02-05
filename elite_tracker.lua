9a83871cd652c51582f9d0f5e8c48264d80bc16f
ShinyWasabi <hgta15746@gmail.com>
2024-02-05 17:46:34 +0300

    redesign the script

--- elite_tracker.lua
+++ elite_tracker.lua
@@ -1,8 +1,9 @@
 elite_tracker_tab = gui.get_tab("Elite Tracker")
 
-h2_tab = elite_tracker_tab:add_tab("The Doomsday Heist")
-ch_tab = elite_tracker_tab:add_tab("The Diamond Casino Heist")
-h4_tab = elite_tracker_tab:add_tab("The Cayo Perico Heist")
+elite_objectives_tab = elite_tracker_tab:add_tab("Elite Objectives")
+
+selected_heist = 0
+view_objectives = false
 
 local og_elites = {}
 local h2_elites = {}
@@ -10,7 +11,6 @@ local ch_elites = {}
 local h4_elite
 
 local has_quick_restarted
-local has_got_detected
 
 local elite_time
 local has_failed_hack
@@ -18,7 +18,8 @@ local kills
 local headshots
 local deaths
 local vehicle_damage
-
+local rashkovsky_damage
+local noose_called
 local h4_elite_time
 local h4_has_failed_hack
 local h4_deaths
@@ -53,19 +54,37 @@ function get_net_difference(timeb)
     return NETWORK.GET_TIME_DIFFERENCE(MISC.GET_GAME_TIMER(), timeb)
 end
 
-function get_mission_time()
-	local mission_time = 0
-	
-	mission_time = get_net_difference(locals.get_int("fm_mission_controller", 19728 + 985))
-	if mission_time <= 0 then
-		mission_time = get_net_difference(locals.get_int("fm_mission_controller", 19728 + 985))
+function draw_text(text)
+    HUD.BEGIN_TEXT_COMMAND_DISPLAY_TEXT("STRING")
+    HUD.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(text)
+    HUD.SET_TEXT_RENDER_ID(1)
+    HUD.SET_TEXT_OUTLINE()
+    HUD.SET_TEXT_WRAP(.0, 0.975)
+    HUD.SET_TEXT_RIGHT_JUSTIFY(true)
+    HUD.SET_TEXT_SCALE(0, .5)
+    HUD.SET_TEXT_FONT(4)
+    HUD.SET_TEXT_COLOUR(255, 255, 255, 240)
+    HUD.END_TEXT_COMMAND_DISPLAY_TEXT(.0, .2, 0)
+end
+
+function og_is_active()
+	for i = 1, 7 do
+		if globals.get_int(4718592 + 126144) == tunables.get_int("WVM_FLOW_ROOTCONTENT_ID_" .. i) then
+			return true
+		end
 	end
-	mission_time = mission_time + 10000
-	if globals.get_int(2684312 + 43 + 55) or globals.get_int(2684312 + 43 + 56) then
-		mission_time = mission_time + globals.get_int(2685249 + 6465)
+	
+	return false
+end
+
+function h2_is_active()
+	for i = 1, 15 do
+		if globals.get_int(4718592 + 126144) == tunables.get_int("FHM_FLOW_ROOTCONTENT_ID_" .. i) then
+			return true
+		end
 	end
 	
-	return mission_time
+	return false
 end
 
 function ch_is_active()
@@ -102,6 +121,31 @@ function ch_is_active()
 	return false
 end
 
+function h4_is_active()
+	for i = 0, 10 do
+		if globals.get_int(4718592 + 126144) == tunables.get_int("H4_ROOT_CONTENT_ID_" .. i) then
+			return true
+		end
+	end
+	
+	return false
+end
+
+function get_mission_time()
+	local mission_time = 0
+	
+	mission_time = get_net_difference(locals.get_int("fm_mission_controller", 19728 + 985))
+	if mission_time <= 0 then
+		mission_time = get_net_difference(locals.get_int("fm_mission_controller", 19728 + 985))
+	end
+	mission_time = mission_time + 10000
+	if globals.get_int(2684312 + 43 + 55) or globals.get_int(2684312 + 43 + 56) then
+		mission_time = mission_time + globals.get_int(2685249 + 6465)
+	end
+	
+	return mission_time
+end
+
 function h4_get_mission_time()
     local mission_time = 0
 
@@ -122,14 +166,40 @@ function h4_get_mission_time()
     return mission_time
 end
 
-function h4_is_active()
-	for i = 0, 10 do
-		if globals.get_int(4718592 + 126144) == tunables.get_int("H4_ROOT_CONTENT_ID_" .. i) then
-			return true
+function is_elite_timer_paused()
+	if selected_event == 3 then
+		return locals.get_int("fm_mission_controller_2020", 48513 + 1487) == 0
+	else
+		return locals.get_int("fm_mission_controller", 19728 + 985) == 0
+	end
+end
+
+function render_elite_objectives()
+	if selected_heist == 0 then
+		if og_is_active() then
+			draw_text("TIME: " .. (not is_elite_timer_paused() and elite_time or "PAUSED") .. "~n~QUICK RESTARTED: " .. (has_quick_restarted ~= 0 and "YES" or "NO") .. "~n~FAILED HACK: " .. (has_failed_hack ~= 0 and "YES" or "NO") .. "~n~DEATHS: " .. deaths .. "~n~KILLS: " .. kills .. "~n~VEHICLE DAMAGE: " .. vehicle_damage .. "%" .. "RASHKOVSKY DAMAGE: " .. rashkovsky_damage .. "%" .. "NOOSE CALLED: " .. noose_called ~= 0 and "YES" or "NO")
+		else
+			draw_text("HEIST IS NOT ACTIVE.")
+		end	
+	elseif selected_heist == 1 then
+		if h2_is_active() then
+			draw_text("TIME: " .. (not is_elite_timer_paused() and elite_time or "PAUSED") .. "~n~QUICK RESTARTED: " .. (has_quick_restarted ~= 0 and "YES" or "NO") .. "~n~FAILED HACK: " .. (has_failed_hack ~= 0 and "YES" or "NO") .. "~n~DEATHS: " .. deaths .. "~n~KILLS: " .. kills .. "~n~HEADSHOTS: " .. headshots .. "~n~VEHICLE DAMAGE: " .. vehicle_damage .. "%")
+		else
+			draw_text("HEIST IS NOT ACTIVE.")
+		end	
+	elseif selected_heist == 2 then
+		if ch_is_active() then
+			draw_text("TIME: " .. (not is_elite_timer_paused() and elite_time or "PAUSED") .. "~n~QUICK RESTARTED: " .. (has_quick_restarted ~= 0 and "YES" or "NO") .. "~n~FAILED HACK: " .. (has_failed_hack ~= 0 and "YES" or "NO") .. "~n~DEATHS: " .. deaths .. "~n~HEADSHOTS: " .. headshots)
+		else
+			draw_text("HEIST IS NOT ACTIVE.")
+		end	
+	elseif selected_heist == 3 then
+		if h4_is_active() then
+			draw_text("TIME: " .. (not is_elite_timer_paused() and h4_elite_time or "PAUSED") .. "~n~QUICK RESTARTED: " .. (has_quick_restarted ~= 0 and "YES" or "NO") .. "~n~FAILED HACK: " .. (h4_has_failed_hack ~= 0 and "YES" or "NO") .. "~n~DEATHS: " .. h4_deaths .. "~n~BAG: $" .. h4_grabbed_cash .. " (" .. h4_bag_size .. "%)")
+		else
+			draw_text("HEIST IS NOT ACTIVE.")
 		end
 	end
-	
-	return false
 end
 
 script.register_looped("Elite Tracker", function(script)
@@ -144,24 +214,24 @@ script.register_looped("Elite Tracker", function(script)
 	ch_elites[1] = stats.get_packed_stat_bool(28194)
 	ch_elites[2] = stats.get_packed_stat_bool(28195)
 	ch_elites[3] = stats.get_packed_stat_bool(28196)
+	h4_elite = stats.get_bool("MPX_AWD_ELITE_THIEF")
 	has_quick_restarted = globals.get_int(2685249 + 6463)
-	if has_bit_set(locals.get_int("fm_mission_controller", 22942 + 1552 + 11), 1) then
-		has_got_detected = true
-	else
-		has_got_detected = false
-	end
 	elite_time = format_milliseconds(get_mission_time())
 	kills = locals.get_int("fm_mission_controller", 19728 + 1725 + 1)
 	headshots = locals.get_int("fm_mission_controller", 19728 + 1740 + 1)
 	deaths = locals.get_int("fm_mission_controller", 19728 + 1730 + 1)
 	has_failed_hack = locals.get_int("fm_mission_controller", 28347 + 3197)
 	vehicle_damage = locals.get_int("fm_mission_controller", 24562 + 1231)
-	h4_elite = stats.get_bool("MPX_AWD_ELITE_THIEF")
+	rashkovsky_damage = locals.get_int("fm_mission_controller", 24562 + 1230)
+	noose_called = has_bit_set(locals.get_int("fm_mission_controller", 19728 + 3), 11) and 1 or 0
 	h4_elite_time = format_milliseconds(h4_get_mission_time())
 	h4_has_failed_hack = locals.get_int("fm_mission_controller_2020", 51882 + 1517 + 51)
 	h4_deaths = locals.get_int("fm_mission_controller_2020", 51882 + 1517 + 36 + 1)
 	h4_bag_size = math.floor(locals.get_float("fm_mission_controller_2020", 60496 + (1 + (0 * 261)) + 236 + 2) / SYSTEM.TO_FLOAT(tunables.get_int(1859395035)) * 100.0)
-	h4_grabbed_cash = locals.get_int("fm_mission_controller_2020", 51882 + 1517 + 53)
+	h4_grabbed_cash = format_int(locals.get_int("fm_mission_controller_2020", 51882 + 1517 + 53))
+	if view_objectives and not HUD.IS_HUD_COMPONENT_ACTIVE(19) then
+		render_elite_objectives()
+	end
 end)
 
 elite_tracker_tab:add_imgui(function()
@@ -182,66 +252,7 @@ elite_tracker_tab:add_imgui(function()
 	ImGui.Text("Cayo Perico: " .. (h4_elite and "completed" or "not completed"))
 end)
 
--- TO-DO: Add if doomsday heist active check and handle different time requirements
-h2_tab:add_imgui(function()
-	if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("fm_mission_controller")) ~= 0 then
-		if locals.get_int("fm_mission_controller", 19728 + 985) ~= 0 then
-			ImGui.Text("Timer: " .. elite_time .. " / 00:15:00")
-		else
-			ImGui.Text("Timer: paused")
-		end
-	else
-		ImGui.Text("Timer: not started")
-	end
-	
-	ImGui.Text("Quick Restarted: " .. (has_quick_restarted ~= 0 and "Yes" or "No"))
-	ImGui.Text("Detected: " .. (has_got_detected and "Yes" or "No"))
-	ImGui.Text("Failed Hack: " .. (has_failed_hack ~= 0 and "Yes" or "No"))
-	ImGui.Text("Deaths: " .. deaths)
-	ImGui.Text("Kills: " .. kills)
-	ImGui.Text("Headshots: " .. headshots)
-	ImGui.Text("Vehicle Damage: " .. vehicle_damage .. "%")
-end)
-
-ch_tab:add_imgui(function()
-	if ch_is_active() then
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("fm_mission_controller")) ~= 0 then
-			if locals.get_int("fm_mission_controller", 19728 + 985) ~= 0 then
-				ImGui.Text("Timer: " .. elite_time .. " / 00:15:00")
-			else
-				ImGui.Text("Timer: paused")
-			end
-		else
-			ImGui.Text("Timer: not started")
-		end
-		
-		ImGui.Text("Quick Restarted: " .. (has_quick_restarted ~= 0 and "Yes" or "No"))
-		ImGui.Text("Detected: " .. (has_got_detected and "Yes" or "No"))
-		ImGui.Text("Failed Hack: " .. (has_failed_hack ~= 0 and "Yes" or "No"))
-		ImGui.Text("Deaths: " .. deaths)
-		ImGui.Text("Headshots: " .. headshots)
-	else
-		ImGui.Text("Heist is not active.")
-	end
-end)
-
-h4_tab:add_imgui(function()
-	if h4_is_active() then
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("fm_mission_controller_2020")) ~= 0 then
-			if locals.get_int("fm_mission_controller_2020", 48513 + 1487) ~= 0 then
-				ImGui.Text("Timer: " .. h4_elite_time .. " / 00:15:00")
-			else
-				ImGui.Text("Timer: paused")
-			end
-		else
-			ImGui.Text("Timer: not started")
-		end
-		
-		ImGui.Text("Quick Restarted: " .. (has_quick_restarted ~= 0 and "Yes" or "No"))
-		ImGui.Text("Failed Hack: " .. (h4_has_failed_hack ~= 0 and "Yes" or "No"))		
-		ImGui.Text("Deaths: " .. h4_deaths)	
-		ImGui.Text("Bag: $" .. format_int(h4_grabbed_cash) .. " (" .. h4_bag_size .. "%)")
-	else
-		ImGui.Text("Heist is not active.")
-	end
+elite_objectives_tab:add_imgui(function()
+	selected_heist = ImGui.Combo("Select Heist", selected_heist, { "OG Heists", "Doomsday Heist", "Diamond Casino Heist", "Cayo Perico Heist" }, 4)
+	view_objectives = ImGui.Checkbox("View Objectives", view_objectives)
 end)
